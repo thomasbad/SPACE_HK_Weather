d469b3b774a28e725a4409bccc1f668b476fa07b
mkaa@HKUPC <thomasgoodboy@gmail.com>
2021-10-04 10:46:03 +0800

    Initial commit

---
+++ app/src/main/java/mad/s20131828/hkweather/MainActivity.kt
@@ -0,0 +1,11 @@
+package mad.s20131828.hkweather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
